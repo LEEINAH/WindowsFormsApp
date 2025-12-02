7a73e25db3ff4917ddcb8ca2a44b599cc591a0a1
LEEINAH <22na7592@gmail.com>
2025-12-02 13:35:10 +0900

    Elephant

---
+++ 2025-12-02/WindowsFormsApp3/Elephant.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+	class Elephant
+	{
+		public int EarSize; // 귀 사이즈
+		public String Name; // 이름 
+
+		public void WhoAmI()
+		{
+			MessageBox.Show("My ears are " + EarSize + " inches tall.",
+				Name + " says...");
+		}
+	}
+}
